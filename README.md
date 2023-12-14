f730d2e99707d0c0ae4016bbe535146e29ea451b
Flyfish233 <ffgh2010@live.cn>
2023-12-14 17:15:47 +0800

    Finish Merger

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-# aokana-tools-rust
+# Aokana Tools 苍彼工具包
+
+[![Build](https://github.com/Flyfish233/aokana-tools-rust/actions/workflows/build.yml/badge.svg)](https://github.com/Flyfish233/aokana-tools-rust/actions/workflows/build.yml)
+
+> I will be the first to fly beyond the sky-
+
+**Aokana Tools** is a suite designed for the visual
+novel [Aokana -Four Rhythms Across the Blue-](https://vndb.org/v12849) (蒼の彼方のフォーリズム), aimed at providing a
+set of tools for extracting game assets and combining segmented CG images, with multithreading capabilities and operates
+independently of external dependencies
+such as `ffmpeg` or `ImageMagick`, ensuring an easy-to-use and light-weight experience.
+
+With the goal of achieving high-performance extraction and elegantly simple code, these tools are developed in Rust.
+
+## Toolkit
+
+- **Unpacker (WIP)**
+
+  The Unpacker facilitates the decryption and extraction of all game assets from .dat files, tailored for the
+  Steam release of Aokana.
+
+    - `Unpacker` specializes in unpack .dat files encrypted with the original method used in [Aokana -Four Rhythms
+      Across the Blue-](https://store.steampowered.com/app/1044620/_/)
+      and [Aokana EXTRA1](https://store.steampowered.com/app/1340130/_EXTRA1/).
+    - `UnpackerEx` is updated to unpack .dat files with the newer encryption method found in [Aokana
+      EXTRA2](https://store.steampowered.com/app/2206340/_EXTRA2/).
+
+- **Merger**
+
+  Aokana features character sprites that are split into several files. The Merger is here to put these pieces back
+  together, reconstructing the full CG images.
+
+  Please note: As the Decryptor is still being WIP, you will need to decrypt your files in advance using the
+  Kotlin/JVM [aokana-tools-kt](https://github.com/Flyfish233/aokana-tools-kt) Unpacker.
+
+    - **How to Use**
+
+  To merge images, you can place the `merge` tool either in the parent directory or alongside your extracted asset
+  folder, then execute it. CG images will be output to the `merged` directory.
+
+  Make sure you've extracted `system.dat`, which contains `vcglist.csv`, as well as all the images that need to combine.
+
+## 概述
+
+> 能比任何人更快，到达天空的彼方——
+
+**苍彼工具包**用来解包 Steam 版本 Galgame《蒼の彼方のフォーリズム》（苍之彼方的四重奏）加密的 .dat 游戏资源文件，并将分离的图像合并成正常
+CG.
+
+本企划旨在提供易于使用的多线程工具来以最快的速度解包和合并图像，并且不使用任何外部依赖，如 `ffmpeg` 或 `ImageMagick`
+来处理图像以保证轻量和兼容。
+
+本仓库使用 Rust 编写，确保最佳性能和精简。
+
+## 工具集
+
+- **解包器（开发中）**
+
+  解包器用于解密和提取`.dat`文件中的所有游戏资源，专为《蒼の彼方のフォーリズム》(苍之彼方的四重奏) Steam 版本设计。
+
+    - `Unpacker` 用于解包 [苍之彼方的四重奏](https://store.steampowered.com/app/1044620/_/)
+      和 [苍之彼方的四重奏 EXTRA1](https://store.steampowered.com/app/1340130/_EXTRA1/) 中使用的加密方法的资源文件。
+    - `UnpackerEx` 用于解包 [苍之彼方的四重奏 EXTRA2](https://store.steampowered.com/app/2206340/_EXTRA2/)
+      中使用的新加密方法的资源文件。
+
+- **合并**
+
+  苍之彼方的四重奏中的 CG 被拆分为多个文件。合并工具用于这些碎片重新组合，重建完整的 CG 图像。
+
+  由于解包器仍在开发中，您需要预先使用 Kotlin/JVM 版本的 [aokana-tools-kt](https://github.com/Flyfish233/aokana-tools-kt)
+  解包器对文件进行解密。
+
+    - **使用方法**
+
+      把 `merge` 放在解包后的文件目录或父目录，运行它即可。CG 将输出于 `merged` 目录。
+
+      确保您已提取了包含`vcglist.csv`的`system.dat`文件，以及需要合并的所有图像。
---
+++ src/bin/merge.rs
@@ -0,0 +1,109 @@
+use std::{fs, io};
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::time::Instant;
+
+use image::{DynamicImage, ImageError, ImageFormat, imageops, RgbaImage};
+use rayon::prelude::*;
+use walkdir::WalkDir;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let start_time = Instant::now();
+    let processed_images = AtomicUsize::new(0);
+    let folder = PathBuf::from("../..");
+    let csv_not_found = "Failed to find vcglist.csv in any subdirectory,
+    you should extract system.dat and other resources into this folder,
+    then rerun this program.";
+    let csv_file_path = WalkDir::new(&folder)
+        .into_iter()
+        .filter_map(Result::ok)
+        .find(|e| e.file_name() == "vcglist.csv")
+        .ok_or(csv_not_found)?
+        .into_path();
+    let cg_list = fs::read_to_string(csv_file_path)?;
+    let total_images = cg_list.lines().count();
+    println!("Total images: {}", total_images);
+    let output_path = folder.join("merged");
+    fs::create_dir_all(&output_path)?;
+    let image_cache: Arc<dashmap::DashMap<String, DynamicImage>> = Arc::new(dashmap::DashMap::new());
+
+    // Process images
+    cg_list.par_lines().try_for_each(|line| -> Result<(), ImageError> {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+        let cg_name = parts[0];
+        let images: Result<Vec<DynamicImage>, ImageError> = parts.iter().skip(1)
+            .map(|&part| {
+                let file_name = format!("{}.webp", part);
+                let cache = image_cache.clone();
+                if let Some(image) = cache.get(part) {
+                    return Ok(image.clone());
+                }
+                let image = find_and_open_image(&folder, &file_name)?;
+                cache.insert(part.to_string(), image.clone());
+                Ok(image)
+            })
+            .collect();
+
+        // Combine images
+        match images {
+            Ok(images) if !images.is_empty() => {
+                let combined_image = combine_images(&images)?;
+                let image_path = output_path.join(format!("{}.{:?}", cg_name, ImageFormat::Png));
+                combined_image.save_with_format(&image_path, ImageFormat::Png)?;
+            }
+            _ => eprintln!("Failed to combine images for {}", cg_name),
+        }
+
+        // Print progress every 10 images
+        let processed = processed_images.fetch_add(1, Ordering::SeqCst) + 1;
+        if processed % 10 == 0 {
+            print_progress(processed, total_images, start_time);
+        }
+
+        Ok(())
+    })?;
+
+    // Print benchmark
+    println!("Processed {} images, total images: {}", processed_images.load(Ordering::SeqCst), total_images);
+    println!();
+    println!("All done. 🏳️‍⚧️🦀");
+
+    Ok(())
+}
+
+fn find_and_open_image(folder: &Path, file_name: &str) -> Result<DynamicImage, ImageError> {
+    let file_path = WalkDir::new(folder)
+        .into_iter()
+        .filter_map(Result::ok)
+        .find(|e| e.file_name() == file_name)
+        .ok_or_else(|| io::Error::new(io::ErrorKind::NotFound, "Image file not found,
+        did you well extracted all the images?"))?
+        .into_path();
+    image::open(file_path)
+}
+
+fn combine_images(images: &[DynamicImage]) -> Result<RgbaImage, ImageError> {
+    images.iter().try_fold(None, |combined, image| {
+        let watermark = image.to_rgba8();
+        match combined {
+            Some(mut base) => {
+                imageops::overlay(&mut base, &watermark, 0, 0);
+                Ok(Some(base))
+            }
+            None => Ok(Some(watermark)),
+        }
+    }).map(|combined| combined.expect("Image not found, did you well extracted all the images?"))
+}
+
+fn print_progress(processed: usize, total_images: usize, start_time: Instant) {
+    let elapsed_time = start_time.elapsed().as_secs_f64();
+    let images_per_second = processed as f64 / elapsed_time;
+    let percent_complete = (processed as f64 / total_images as f64) * 100.0;
+    println!(
+        "Processed: {}/{} ({:.2}%), Time: {:.2}s, Rate: {:.2} images/s, Remaining: {:.0}m:{:.0}s",
+        processed, total_images, percent_complete, elapsed_time, images_per_second,
+        ((total_images - processed) as f64 / images_per_second) / 60.0,
+        ((total_images - processed) as f64 / images_per_second) % 60.0,
+    );
+}
